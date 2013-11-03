2f0e2c174951ead0b09ab1e70b40005f3f3cc7d8
Yoshiaki Nakanishi <nakanisi@brightechno.com>
2013-11-03 13:20:13 +0900

    各画面用の空Luaファイルを登録

---
+++ attack-on-deadline/Login.lua
@@ -0,0 +1,16 @@
+--[[
+ログイン用画面
+
+概要：
+・名前が入力済みでOK押したらサーバに要求
+・サーバから正常な結果が返ってきたら、ルーム選択画面に遷移する
+]]
+
+function setup()
+end
+
+function execute(deltaT)
+end
+
+function leave()
+end
---
+++ attack-on-deadline/Lose.lua
@@ -0,0 +1,16 @@
+--[[
+敗北画面 (そもそも敗北条件あるのか？)
+
+概要：
+・敗北した表示をする画面
+・戻るボタンを押したらルーム選択画面に画面遷移する
+]]
+
+function setup()
+end
+
+function execute(deltaT)
+end
+
+function leave()
+end
