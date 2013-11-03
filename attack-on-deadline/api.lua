fe5ca628d4257ee9e7ccc3e4dfc06e1d6d59963a
Masahiro Wakame <vvakame@gmail.com>
2013-11-03 13:41:26 +0900

    🙋 shinchoku.api.fetchRoomsのガワを作成した

--- attack-on-deadline/api.lua
+++ attack-on-deadline/api.lua
@@ -15,18 +15,23 @@ function apiLoad()
 		return "http://54.238.127.127" .. path
 	end
 
+	-- 使い方
+	-- api.fetchRooms({}, callback) callback は Function
 	api.fetchRooms = function (params, callback)
 		syslog("[API] called fetchRooms")
 
 		local json = CONV_Lua2Json(params)
 		local timestamp = ENG_getNanoTime()
 		local callbackName = "SHINCHOKU_CALLBACK_fetchRooms_" .. timestamp
-		_G[callbackName] = function()
+		_G[callbackName] = function(connectionID, message, status, bodyPayload)
 			syslog("callback is coming! " .. callbackName)
+			_G[callbackName] = nil
+			callback(connectionID, message, status, bodyPayload)
 		end
 
 		local pHTTP = HTTP_API(callbackName)
-		sysCommand(pHTTP, NETAPI_SEND, buildUrl("/room.php"), params, json, 30000)
+		sysCommand(pHTTP, NETAPI_SEND, "https://dl.dropboxusercontent.com/u/6581286/sample.json", params, json, 30000)
+		return pHTTP
 	end
 end
 apiLoad()
