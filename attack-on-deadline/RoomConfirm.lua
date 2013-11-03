249eaa26cdd6b4e0bdf8c27fbeffb968559e413a
Yoshiaki Nakanishi <nakanisi@brightechno.com>
2013-11-03 16:09:04 +0900

    ルーム情報表示にサーバとの通信処理を追加

--- attack-on-deadline/RoomConfirm.lua
+++ attack-on-deadline/RoomConfirm.lua
@@ -11,10 +11,47 @@ function setup()
 	if not rootTbl then
 		rootTbl = {}
 	end
+
+	serverResult = nil -- ここにサーバから返ってきた bodyPayload が入る
+	status = 0 -- まだサーバに何もリクエストしてないよ
+
+	shinchoku.api.fetchRoomInfo(rootTbl.user.id, callbackFetchRoomInfo)
+
+	status = 1 -- サーバへのリクエスト送ったよ
 end
 
 function execute(deltaT)
+	if status == 1 or status == 0 then
+		-- TODO: あんまり通信中状態が長かったらキャンセルする処理も入れた方がいいかも
+		return
+	end
+	if status == -1 then
+		networkError()
+
+		-- TODO: 前画面かトップ画面に戻る処理を入れる
+		return
+	end
+
+	-- ここには通信成功 (status == 2)でしかこないはず
+	status = 0 -- 通信終わってるはずだから元に戻しておくよ
+
+	-- TODO: ルーム情報を表示する
 end
 
 function leave()
+end
+
+-- サーバとの通信処理のコールバック
+function callbackFetchRoomInfo(connectionID, message, status, bodyPayload)
+	if message ~= NETAPIMSG_REQUEST_SUCCESS or status ~= 200 then
+		status = -1 -- 通信エラーだよ
+		return
+	end
+
+	status = 2 -- サーバとの処理成功したよ
+	serverResult = bodyPayload
+end
+
+-- TODO: エラー処理を書く
+function networkError()
 end
